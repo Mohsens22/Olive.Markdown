550ecbcb34567429d612c1de1e88d8f2d58911d1
Mohsen Seifi <mohsens2@outlook.com>
2018-04-07 01:49:26 +0430

    Added some initial renderer codes

---
+++ Olive.Markdown/Olive.Markdown/MarkdownEngine.cs
@@ -0,0 +1,18 @@
+﻿using RazorLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Olive.Markdown
+{
+    public class MarkdownEngine
+    {
+        internal static readonly RazorLightEngineBuilder Engine;
+        public void Build()
+        {
+            Engine = new RazorLightEngineBuilder()
+              .UseMemoryCachingProvider()
+              .Build();
+        }
+    }
+}
---
+++ Olive.Markdown/Olive.Markdown/Test.cs
@@ -0,0 +1,20 @@
+﻿using RazorLight;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olive.Markdown
+{
+    public class Test
+    {
+        public static async void RenderBitch()
+        {
+            
+
+
+            string result = await engine.CompileRenderAsync("templateKey", File.ReadAllText(@"C:\Users\Mohsen\Documents\Visual Studio 2017\Projects\MDEngineTest\MDEngineTest\Templates\GCop.csmd"), new { Name = "John Doe" });
+        }
+    }
+}
